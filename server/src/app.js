38e4a9fe7a2b8f5df72ccdba3965f00c4d6c1841
Rishabh Saluja <185166851+rishabhsaluja1@users.noreply.github.com>
2025-12-19 03:32:15 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -31,4 +31,10 @@ app.get("/protected", authenticate, (req, res) => {
   });
 });
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
 module.exports = app;
